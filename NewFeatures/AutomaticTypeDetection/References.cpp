0b41f4265be5ecf105f905582526c047330aa23f
Ashish-Surve <ashish_surve@outlook.in>
2020-01-24 17:31:41 +0530

    Updated References Topic

--- NewFeatures/AutomaticTypeDetection/References.cpp
+++ NewFeatures/AutomaticTypeDetection/References.cpp
@@ -17,4 +17,46 @@ int main()
 	cout << "Data Address : " << &Data << endl; 
 
 	cout << "Ref Address : " << ref << endl;
+
+
+	cout << "----------------------------------------" << endl;
+	//reference to an array
+
+	int arr[5] = { 1,2,3,4,5 };
+	//																					--IMPORTANT
+	int (&ref2)[5] = arr;
+	cout << ref2[3] << endl;
+
+	//Range based loop  c++11
+
+	cout << "----------------------------------------" << endl;
+
+	for (int i : ref2)
+	{
+		cout << i << endl;
+	}
+
+
+	cout << "----------------------------------------" << endl;
+
+	//Pointer to a reference- not possibke
+
+	int dat = 99;
+	int &ref4 = dat;
+	int* ptr = &ref4;
+
+	cout << *ptr << endl;
+	ref4 = 85;
+	cout << *ptr << endl;
+
+
+	//Array of reference
+		//Not possible
+
+
+	//reference to a reference
+	int &ref5 = ref4;
+
+
+
 }
